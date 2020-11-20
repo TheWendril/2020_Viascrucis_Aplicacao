d34e000dfae67a686b72728909c87223f9b49e6a
TheWendril <wendril2001avila@gmail.com>
2020-11-20 16:35:36 -0300

    refactoring gallery

---
+++ frontend/src/components/amplied_image.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="background-blur text-center" @click="hidden_content">
+        <div class="row mh-100 d-inline-block h-100">
+            <div class="col-md-12 col-sm-4 mh-100 h-100 mx-auto d-flex flex-column justify-content-center align-items-center">
+                <div>
+                    <img class="img-fluid " :src="font">
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+
+<script>
+export default {
+    props: {
+        font: String
+    },
+    methods: {
+        hidden_content: function(){
+            this.$emit('hiddenEvent')
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+
+
+.background-blur{
+    animation: amplier 500ms;
+    width: 100%;
+    height: 100vh;
+    position: fixed;
+    background-color: rgb(0, 0, 0, 0.7);    
+    z-index: 333;
+    top: 0;
+
+}
+
+
+@keyframes amplier {
+    0%{
+        opacity: 0;
+    }
+    100%{
+        opacity: 100%;
+    }
+}
+
+</style>
--- frontend/src/components/gallery.vue
+++ frontend/src/components/gallery.vue
@@ -1,98 +1,114 @@
 <template>
+<div>
+  <amplied_image @hiddenEvent="hidden_amplied_image" :font="atual_link_amplied" v-if="amplied"/>
+  <div :class="{'container': true, 'hasblur': amplied}">
 
-<div class="container">
+    <h1 class="font-weight-light text-center text-lg-left mt-4 mb-0">Galeria</h1>
 
-  <h1 class="font-weight-light text-center text-lg-left mt-4 mb-0">Galeria</h1>
+    <hr class="mt-2 mb-5">
 
-  <hr class="mt-2 mb-5">
+    <div class="row text-center text-lg-left">
 
-  <div class="row text-center text-lg-left">
+      <div class="col-lg-3 col-md-4 col-6" v-for="img in imgs" :key="img.position" >
+        
+        <a @click.once="increment_upvote(img)"> 
+          <img src="../assets/up-arrow.png" class="mb-1" width="17px" height="20px" >
+          {{img.upvotes}} Up Votes  
+        </a>
 
-    <div class="col-lg-3 col-md-4 col-6">
-      <a href="#" class="d-block mb-4 h-100">
-            <img class="img-fluid img-thumbnail" src="https://source.unsplash.com/pWkk7iiCoDM/400x300" alt="">
-          </a>
-    </div>
-    <div class="col-lg-3 col-md-4 col-6">
-      <a href="#" class="d-block mb-4 h-100">
-            <img class="img-fluid img-thumbnail" src="https://source.unsplash.com/aob0ukAYfuI/400x300" alt="">
-          </a>
-    </div>
-    <div class="col-lg-3 col-md-4 col-6">
-      <a href="#" class="d-block mb-4 h-100">
-            <img class="img-fluid img-thumbnail" src="https://source.unsplash.com/EUfxH-pze7s/400x300" alt="">
-          </a>
-    </div>
-    <div class="col-lg-3 col-md-4 col-6">
-      <a href="#" class="d-block mb-4 h-100">
-            <img class="img-fluid img-thumbnail" src="https://source.unsplash.com/M185_qYH8vg/400x300" alt="">
-          </a>
-    </div>
-    <div class="col-lg-3 col-md-4 col-6">
-      <a href="#" class="d-block mb-4 h-100">
-            <img class="img-fluid img-thumbnail" src="https://source.unsplash.com/sesveuG_rNo/400x300" alt="">
-          </a>
-    </div>
-    <div class="col-lg-3 col-md-4 col-6">
-      <a href="#" class="d-block mb-4 h-100">
-            <img class="img-fluid img-thumbnail" src="https://source.unsplash.com/AvhMzHwiE_0/400x300" alt="">
-          </a>
-    </div>
-    <div class="col-lg-3 col-md-4 col-6">
-      <a href="#" class="d-block mb-4 h-100">
-            <img class="img-fluid img-thumbnail" src="https://source.unsplash.com/2gYsZUmockw/400x300" alt="">
-          </a>
-    </div>
-    <div class="col-lg-3 col-md-4 col-6">
-      <a href="#" class="d-block mb-4 h-100">
-            <img class="img-fluid img-thumbnail" src="https://source.unsplash.com/EMSDtjVHdQ8/400x300" alt="">
-          </a>
-    </div>
-    <div class="col-lg-3 col-md-4 col-6">
-      <a href="#" class="d-block mb-4 h-100">
-            <img class="img-fluid img-thumbnail" src="https://source.unsplash.com/8mUEy0ABdNE/400x300" alt="">
-          </a>
-    </div>
-    <div class="col-lg-3 col-md-4 col-6">
-      <a href="#" class="d-block mb-4 h-100">
-            <img class="img-fluid img-thumbnail" src="https://source.unsplash.com/G9Rfc1qccH4/400x300" alt="">
-          </a>
-    </div>
-    <div class="col-lg-3 col-md-4 col-6">
-      <a href="#" class="d-block mb-4 h-100">
-            <img class="img-fluid img-thumbnail" src="https://source.unsplash.com/aJeH0KcFkuc/400x300" alt="">
-          </a>
-    </div>
-    <div class="col-lg-3 col-md-4 col-6">
-      <a href="#" class="d-block mb-4 h-100">
-            <img class="img-fluid img-thumbnail" src="https://source.unsplash.com/p2TQ-3Bh3Oo/400x300" alt="">
-          </a>
-    </div>
-  </div>
+        <a href="#" class="d-block mb-4 h-100" >
+          <img class="img-fluid img-thumbnail" :src="img.link" @click="show_amplied_image(img.link)">
+        </a>
 
-</div>    
+      </div>
+    </div>
+
+  </div>    
+  
+</div>
 </template>
 
 <script>
 
 import { mapMutations } from 'vuex'
+import Axios from 'axios'
+import amplied_image from './amplied_image.vue'
 
 export default {
 
+  components:{
+    amplied_image
+  },
+
+  data(){
+    return{
+
+      amplied: false,
+      atual_link_amplied: '',
+      imgs: []
+    }
+  },
+
   methods: {
-    ...mapMutations(['enable'])
+    ...mapMutations(['enable']),
+    
+    increment_upvote: function(img){
+      
+      console.log(img)
+      
+      var put_request = {
+        position: img.position
+      }
+
+      Axios.put('http://localhost:3000/gallery', {put_request})
+      img.upvotes += 1
+    },
+
+    hidden_amplied_image: function(){
+      this.amplied = false
+    },
+
+    show_amplied_image: function(link){
+
+      console.log('evento ocorrendo')
+      this.atual_link_amplied = link
+      this.amplied = true
+    }
+
   },
   created(){
+    Axios.get('http://localhost:3000/gallery').then(res => {
+      
+      res.forEach(element => {
+        this.imgs.push(element)
+      });
+    
+    })
+    
     this.enable(1)
+  
+  },
+  
+  computed : {
+
   }
+
 
 }
 </script>
 
 <style scoped>
 
+.hasblur{
+  filter: blur(4px);
+}
+
 .container{
     margin-top: 110px;
+}
+
+a:hover{
+  cursor: pointer;
 }
 
 </style>
