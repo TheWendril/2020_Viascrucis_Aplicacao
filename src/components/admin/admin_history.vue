20b8c00a18fe06877658a4714799435dbc70c98f
TheWendril <wendril2001avila@gmail.com>
2020-12-10 01:24:22 -0300

    fix patch

--- src/components/admin/admin_history.vue
+++ src/components/admin/admin_history.vue
@@ -46,7 +46,7 @@
                         <b>Sobrenome:</b> {{story.lastNameHonor}}
                     </div>
                     <div class="col">
-                        <b>Ano de falescimento:</b> {{story.anoFalecimento}}
+                        <b>Ano de falecimento:</b> {{story.anoFalecimento}}
                     </div>
                     <div class="col">
                         <b>Ano de nascimento:</b> {{story.anoNascimento}}
